bbc46735a6c4f7f7767f99e05273cdba0e3d1359
Alexandre Díaz <dev@redneboa.es>
2016-08-01 05:11:01 +0200

    [8.0][IMP][website_google_apis] All updated to 8.0 Odoo API, added OAuth 2.0 Key

--- website_google_apis/models/res_config.py
+++ website_google_apis/models/res_config.py
@@ -1,39 +1,39 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C)2004-TODAY Tech Receptives(<https://www.techreceptives.com>)
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 Solucións Aloxa S.L. <info@aloxa.eu>
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
+#    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from openerp import models, fields
 
 
-class website_config_settings(osv.osv_memory):
+class website_config_settings(models.Model):
     _inherit = 'website.config.settings'
-
-    _columns = {
-        'recaptcha_site_key': fields.related(
-            'website_id', 'recaptcha_site_key', type="char",
-            string='reCAPTCHA site Key'),
-        'recaptcha_private_key': fields.related(
-            'website_id', 'recaptcha_private_key', type="char",
-            string='reCAPTCHA Private Key'),
-        'google_maps_key': fields.related(
-            'website_id', 'google_maps_key', type="char",
-            string='Google Maps Key')
-    }
-
+    
+    recaptcha_site_key = fields.Char(
+        related='website_id.recaptcha_site_key',
+        string='reCAPTCHA Site Key')
+    recaptcha_private_key = fields.Char(
+        related='website_id.recaptcha_private_key',
+        string='reCAPTCHA Private Key')
+    google_maps_key = fields.Char(
+        related='website_id.google_maps_key',
+        string='Google Maps Key')
+    google_oauth2 = fields.Char(
+        related='website_id.google_oauth2',
+        string='Google OAuth 2.0 Key')
