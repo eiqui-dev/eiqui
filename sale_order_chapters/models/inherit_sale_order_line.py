111e0430bd0db624535b64e50ccb175432277710
Alexandre Díaz <dev@redneboa.es>
2017-01-19 00:54:49 +0100

    [FIX] Only update chapter when change product_id

--- sale_order_chapters/models/inherit_sale_order_line.py
+++ sale_order_chapters/models/inherit_sale_order_line.py
@@ -30,15 +30,16 @@ class sale_order_line(osv.osv):
             uom=uom, qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
             lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging, 
             fiscal_position=fiscal_position, flag=flag, context=context)
-        partner_obj = self.pool.get('res.partner')
-        product_obj = self.pool.get('product.product')
-        partner = partner_obj.browse(cr, uid, partner_id)
-        lang = partner.lang
-        context_partner = context.copy()
-        context_partner.update({'lang': lang, 'partner_id': partner_id})
-        product_obj = product_obj.browse(cr, uid, product, context=context_partner)
-        if product_obj:
-            res['value'].update({'chapter_id': product_obj.chapter_id.id or False})
+        if not flag:
+            partner_obj = self.pool.get('res.partner')
+            product_obj = self.pool.get('product.product')
+            partner = partner_obj.browse(cr, uid, partner_id)
+            lang = partner.lang
+            context_partner = context.copy()
+            context_partner.update({'lang': lang, 'partner_id': partner_id})
+            product_obj = product_obj.browse(cr, uid, product, context=context_partner)
+            if product_obj:
+                res['value'].update({'chapter_id': product_obj.chapter_id.id or False})
         return res
             
     def get_name_description(self, cr, uid, ids, context=None):
